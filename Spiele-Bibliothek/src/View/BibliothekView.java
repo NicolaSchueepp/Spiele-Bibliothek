c6ca6a84368c26522a77a915a70f63aa9ceebc51
zkarab <zkarab@WS-ZKARAB.bbcnet-zh.local>
2018-02-22 16:33:26 +0100

    End of Day

--- Spiele-Bibliothek/src/View/BibliothekView.java
+++ Spiele-Bibliothek/src/View/BibliothekView.java
@@ -1,11 +1,13 @@
 package View;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.UIManager;
 
 import model.Benutzer;
 
@@ -14,25 +16,38 @@ public class BibliothekView extends viewSuperclass {
 	private static final long serialVersionUID = 1L;
 	private JButton download = new JButton("Download");
 	private JButton download2 = new JButton("Download");
-	private JLabel logo = new JLabel(loadIcon("Logo.png"));
+	private JPanel cod = new JPanel(loadIcon("callOfDutyWWII.jpg"));
 	private JLabel assasinsCreed = new JLabel(loadIcon("assassinsCreedOrigins.jpg"));
 	private JLabel assasinsCreedText = new JLabel("TEXT");
-	private JLabel logoText = new JLabel("TEXT2");
+	private JLabel codText = new JLabel("TEXT2");
 
 	public BibliothekView(Benutzer benutzer) {
 		setTitle("Bibliothek");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		addMainMenu(benutzer);
+	
+		final JPanel images = new JPanel(new GridLayout(1, 1));
+		images.add(cod);
+		images.add(assasinsCreed);
 		
-		final JPanel gameView = new JPanel(new GridLayout(3, 1));
-		gameView.add(logo);
-		gameView.add(assasinsCreed);
-		gameView.add(logoText);
-		gameView.add(assasinsCreedText);
-		gameView.add(download);
-		gameView.add(download2);
 		
-		add(gameView, BorderLayout.SOUTH);
+		final JPanel downloads = new JPanel(new GridLayout(1, 1));
+		downloads.add(download);
+		downloads.add(download2);
+		
+		add(images, BorderLayout.CENTER);
+		add(downloads, BorderLayout.SOUTH);
+		
+		cod.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		        cod.setBackground(Color.GREEN);
+		    }
+		    
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		        cod.setBackground(UIManager.getColor("control"));
+		    }
+		});
+
 		
 		
 	}
