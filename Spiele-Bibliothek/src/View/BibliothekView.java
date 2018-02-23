6021f33ebca1cd2516abba3cc45c5142819562c3
zkarab <zkarab@WS-ZKARAB.bbcnet-zh.local>
2018-02-23 09:25:46 +0100

    Bibliothek edit

--- Spiele-Bibliothek/src/View/BibliothekView.java
+++ Spiele-Bibliothek/src/View/BibliothekView.java
@@ -2,7 +2,10 @@ package View;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -16,7 +19,7 @@ public class BibliothekView extends viewSuperclass {
 	private static final long serialVersionUID = 1L;
 	private JButton download = new JButton("Download");
 	private JButton download2 = new JButton("Download");
-	private JPanel cod = new JPanel(loadIcon("callOfDutyWWII.jpg"));
+	private JLabel cod = new JLabel(loadIcon("callOfDutyWWII.jpg"));
 	private JLabel assasinsCreed = new JLabel(loadIcon("assassinsCreedOrigins.jpg"));
 	private JLabel assasinsCreedText = new JLabel("TEXT");
 	private JLabel codText = new JLabel("TEXT2");
@@ -29,25 +32,33 @@ public class BibliothekView extends viewSuperclass {
 		final JPanel images = new JPanel(new GridLayout(1, 1));
 		images.add(cod);
 		images.add(assasinsCreed);
+		images.add(download);
 		
+//		final JPanel downloads = new JPanel(new GridLayout(1, 1));
+//		downloads.add(download);
 		
-		final JPanel downloads = new JPanel(new GridLayout(1, 1));
-		downloads.add(download);
-		downloads.add(download2);
-		
-		add(images, BorderLayout.CENTER);
-		add(downloads, BorderLayout.SOUTH);
-		
-		cod.addMouseListener(new java.awt.event.MouseAdapter() {
-		    public void mouseEntered(java.awt.event.MouseEvent evt) {
-		        cod.setBackground(Color.GREEN);
-		    }
-		    
-		    public void mouseExited(java.awt.event.MouseEvent evt) {
-		        cod.setBackground(UIManager.getColor("control"));
-		    }
+		download.setVisible(false);
+
+		cod.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent e) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+				download.setVisible(false);
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+				download.setVisible(true);
+				repaint();
+			}
 		});
 
+		
+		
+		add(images, BorderLayout.CENTER);
+//		add(downloads, BorderLayout.SOUTH);
+		
 		
 		
 	}
