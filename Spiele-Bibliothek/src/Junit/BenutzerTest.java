8e8b9bf4c122940452dc91c196f7204b41651b9b
zkarab <zkarab@WS-ZKARAB.bbcnet-zh.local>
2018-02-26 13:31:42 +0100

    BenutzerTest

---
+++ Spiele-Bibliothek/src/Junit/BenutzerTest.java
@@ -0,0 +1,31 @@
+package Junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import model.Benutzer;
+
+public class BenutzerTest {
+	private static Benutzer a;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		BenutzerTest.a = new Benutzer();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		BenutzerTest.a = null;
+	}
+
+	@Test
+	public void testFindUserByName() {
+		final String name = "abcd";
+		Assert.assertEquals("abcd",name);
+	}
+
+}
